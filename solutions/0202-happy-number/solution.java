38764d96cdbf0480e5a4927edf98a7c4a4add333
Adarsh Bhagwat Shinde <202401040006@mitaoe.ac.in>
2026-08-15 07:17:01 +0530

    ✅ Add solution: Happy Number

---
+++ solutions/0202-happy-number/solution.java
@@ -0,0 +1,26 @@
+class Solution { 
+    public static int sum(int n) {
+        int sum=0;
+        while (n>0) {
+            int d=n%10;
+            n=n/10;
+            sum += (d*d);
+        }
+        return sum;
+    } 
+
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while (fast!=1) {
+            slow = sum(slow);
+            fast = sum(fast);
+            fast = sum(fast);
+            if (slow==fast && slow!=1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
