69ad4dffd637f8668a6b7ceea6deca60d9d81fde
Adarsh Bhagwat Shinde <202401040006@mitaoe.ac.in>
2026-08-26 12:23:20 +0530

    ✅ Add solution: Minimum Size Subarray Sum

--- solutions/0209-minimum-size-subarray-sum/solution.java
+++ solutions/0209-minimum-size-subarray-sum/solution.java
@@ -1,58 +1,20 @@
 class Solution {
-	public static int max_array (int[] arr) {
-		int max = arr[0];
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] > max) {
-				max = arr[i];
+    public static int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+		int result = Integer.MAX_VALUE;
+		int low=0, high=0, sum=0;
+
+		while (high < n) {
+			sum += nums[high];
+
+			while (sum >= target) {
+				int len = high - low +1;
+				result = Math.min(len, result);
+				sum -= nums[low];
+				low++;
 			}
+			high++;
 		}
-		return max;
-	}
-
-    public static boolean maxSubarraySum(int[] arr, int k, int target) {
-		// if (k<2) {
-		// 	return ;
-		// }
-	
-
-        int n = arr.length;
-		int sum=0, low = 0, high = k-1;
-		
-		for (int i=low; i<=high; i++) {
-			sum += arr[i];
-		}
-
-        if (sum == target) return true;
-
-		int newsum = 0;
-		low ++; high++;
-
-		while (high != n) {
-			newsum = sum - arr[low-1] + arr[high];
-			sum = newsum;
-			low++; high++;
-            if (sum == target) return true;
-		}
-        return false;
-    }
-
-    public int minSubArrayLen(int target, int[] nums) {
-        if (target==11 && nums[2]!=1) return 3;
-        if (target==20) return 2;
-        if (target==213) return 8;
-        if (target==6) return 1;
-        if (target==15 && nums[1]!=2) return 2;
-        if (target==697439) return 132;
-        if (target==396893380) return 79517;
-        if (target==1000000000) return 100000;
-        if (target==7 && nums[0]==8) return 1;
-        if (target==50000) return 50000;
-        if (target==10) return 3;
-        int size = nums.length;
-        int result_sum = 0;
-        for (int i=1; i<=size; i++) {
-            if (maxSubarraySum(nums, i, target)) return i;
-        }
-        return 0;
+        return result == Integer.MAX_VALUE ? 0 : result;
     }
 }
